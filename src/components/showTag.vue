5096a85e1d9d13199fbb9afa8c34608e174d7c73
[Villion_DuHh] <252860883@qq.com>
2017-12-26 21:56:55 +0800

    修改个人资料样式

---
+++ src/components/showTag.vue
@@ -0,0 +1,61 @@
+<template>
+<transition name="error">
+  <div class="error">
+      <div class="con">
+          <span>{{msg}}</span>
+      </div>
+  </div>
+</transition>
+
+</template>
+
+<script>
+export default {
+  props: ['msg'],
+  data() {
+    return {
+      errMsg: this.errorMsg,
+    };
+  },
+  watch:{
+      errorMsg(){
+          this.errMsg=this.errorMsg;
+      }
+  }
+};
+</script>
+
+<style lang="scss">
+.error {
+  z-index: 100;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  margin: auto;
+  .con {
+    background: rgba(0, 0, 0, 0.7);
+    padding: 15px;
+    border-radius: 5px;
+    line-height: 1;
+    img {
+      vertical-align: middle;
+      width: 50px;
+      height: 50px;
+    }
+    span {
+      vertical-align: middle;
+      display: inline-block;
+      color: #ddd;
+      font-size: 16px;
+      font-weight:400;
+    }
+  }
+}
+.error-enter,.error-leave-to{
+    opacity: 0;
+}
+.error-enter-active,.error-leave-active{
+    transition: all .5s;
+}
+</style>
--- src/components/userChange.vue
+++ src/components/userChange.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="user-change">
     <div class="user-con">
+      <a class="btn-sub" @click="submit">确定</a>
+      <div class="photo">设置头像</div>
       <div class="user-row">
         <span>姓名</span>
         <input type="text" v-model="name">        
@@ -13,44 +15,98 @@
         <span>专业</span>
         <input type="text" v-model="major">        
       </div>
+      <div class="user-row">
+        <span>学号</span>
+        <input type="text" v-model="stuId">        
+      </div>
+      
     </div>
+    <!-- 提示 -->
+    <show-tag :msg="msg" v-show="isShowMsg"></show-tag>
   </div>
 </template>
 <script>
+import showTag from "../components/showTag.vue";
 export default {
   data() {
     return {
       name: "杜杜杜",
       school: "123",
-      major: "233"
+      major: "233",
+      stuId: "1411651104",
+      msg: "这是一个提示",
+      isShowMsg: false
     };
+  },
+  components: {
+    showTag
+  },
+  methods: {
+    submit() {
+      let self = this;
+      if (this.name && this.school && this.major && this.stuId) {
+      } else {
+        this.msg = "输入不完整，请填写完整后再提交";
+        this.isShowMsg = true;
+        setTimeout(() => {
+          self.isShowMsg = false;
+        }, 2000);
+      }
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
 @import "../assets/common.scss";
 .user-change {
-  width: 1000px;
-  margin: 0 auto;
-  margin-bottom: 60px;
+  width: 400px;
+  margin: 20px auto;
   box-sizing: border-box;
-  // background: $blank;
-  color:$black;
+  background: $blank;
+  border-radius: 5px;
+  padding: 20px;
+  color: $black;
   .user-con {
-    .user-row{
+    .photo {
+      width: 100px;
+      height: 100px;
+      background: $light;
+      border-radius: 50%;
+      margin: 20px auto;
+      text-align: center;
+      line-height: 100px;
+      color: #fff;
+    }
+    .user-row {
       width: 300px;
       margin: 10px auto;
-      span{
+      span {
         font-size: 20px;
-        margin-right:30px; 
+        margin-right: 5px;
         vertical-align: middle;
       }
-      input{
-        height: 30px;
-        width: 200px;
-        border-radius: 3px;
-        border: 1px solid $light;
+      input {
+        height: 40px;
+        width: 230px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+        outline: none;
+        padding-left: 10px;
       }
+    }
+    .btn-sub {
+      display: block;
+      width: 60px;
+      margin: 0 auto;
+      margin-right: 0;
+      height: 35px;
+      background: $blue;
+      color: #fff;
+      text-align: center;
+      line-height: 35px;
+      border-radius: 5px;
+      cursor: pointer;
+      font-size: 14px;
     }
   }
 }
