783af174a29e8c5e0157660155fc95a1f92ffa7a
[Villion_DuHh] <252860883@qq.com>
2018-02-28 23:34:46 +0800

    [添加]路由接口配置

--- server/router.js
+++ server/router.js
@@ -1,25 +1,56 @@
 "use strict";
 
-const Koa =require('koa');
-const Router=require('koa-router');
-let app=new Koa();
-let router =new Router();
-
-console.log()
-
-// 自习室列表信息
-router.get('/roomlist', (ctx,next)=>{
-    ctx.body='默认的信息';
-});
-
-//
+const Koa = require('koa');
+const Router = require('koa-router');
+let app = new Koa();
+let router = new Router();
 
 // 登陆接口
-router.get('/login', (ctx,next)=>{
-    ctx.body="这是登陆接口"
+router.post('/login', (ctx, next) => {
+    ctx.body = "这是登陆接口"
 });
 
+// 注册接口
+router.post('/register', (ctx, next) => {
+    ctx.body = "这是注册接口"
+});
+
+// 自习室列表信息
+router.get('/roomlist', (ctx, next) => {
+    ctx.body = '自习室列表信息';
+});
+
+// 提醒
+router.get('/remind', (ctx, next) => {
+    ctx.body = '提醒';
+});
+
+// 加入自习
+router.get('/addRoom', (ctx, next) => {
+    ctx.body = '加入自习';
+})
+
+// 加入收藏
+router.get('/addStar', (ctx, next) => {
+    ctx.body = '加入收藏';
+})
+
+// 成为管理员
+router.get('/addAdmin',(ctx)=>{
+    ctx.body="成为管理员";
+})
+
+// 用户信息
+router.get('/user',(ctx)=>{
+    ctx.body="用户信息";
+})
+
+// 已预约自习室
+router.get('/hasRoomlists',(ctx)=>{
+    ctx.body="已预约自习室";
+})
+
 app.use(router.routes());
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log('server is running at 4000');
 });
