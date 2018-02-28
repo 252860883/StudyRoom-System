cace63a761aa9e1497881f87eb1c88306a507093
honghui <252860883@qq.com>
2018-02-28 10:28:09 +0800

    配置路由信息

--- server/router.js
+++ server/router.js
@@ -1,6 +1,21 @@
+"use strict";
+
 const Koa =require('koa');
 const Router=require('koa-router');
 let app=new Koa();
 let router =new Router();
 
-// router.post('/')
+console.log()
+
+router.get('/', (ctx,next)=>{
+    ctx.body='默认的信息';
+});
+
+router.get('/login', (ctx,next)=>{
+    ctx.body="这是登陆接口"
+});
+
+app.use(router.routes());
+app.listen(4000,()=>{
+    console.log('server is running at 4000');
+});
